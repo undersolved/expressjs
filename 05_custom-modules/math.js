686df9c698c141b4a8915ca3ec95503054cbfe3e
Undersolved <singh.bhupendra.h@gmail.com>
2026-08-04 17:38:06 +0530

    custom modules

---
+++ 05_custom-modules/math.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+	return a + b;
+}
+function sub(a, b) {
+	return a - b;
+}
+function mul(a, b) {
+	return a * b;
+}
+function div(a, b) {
+	return a / b;
+}
+
+/* -------------------------------------------------------------------------- */
+/*
+Two ways to expor this module
+1. Named export ---> if we want to export a specific function individually
+2. Default export ---> if we want to export this full file as a module
+*/
+/* -------------------------------------------------------------------------- */
